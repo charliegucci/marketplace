9bd9faea968134b4f74387f7dc19314d51b52524
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-02-19 17:05:38 +1000

    Installled+Edited devise views and navbar
    ... and used Bulma to prettify the html :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  default_form_builder BulmaFormBuilder
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :location, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :location, :address])
+  end
 end
+

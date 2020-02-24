79e7b74279bdd9752193c4ad6e01d49c35377c45
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-02-24 23:11:19 +0800

    Styles notifications and add behavior for the .delete button

---
+++ app/assets/javascripts/bulma-notifications.js
@@ -0,0 +1,11 @@
+// Removes Bulma Notifications
+// Source: https://bulma.io/documentation/elements/notification/#javascript-example
+document.addEventListener('DOMContentLoaded', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});

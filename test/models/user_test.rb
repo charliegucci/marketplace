7c560b3a8cfdf684c701ec45f350ef7e15300fc5
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-03-02 15:12:49 +0800

    Add validation for User#application_status
    Valid values are:
    not_applied
    applied
    approved
    rejected
    completed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def valid_user
+    User.create(email: 't@g.com',
+                password: 'asdfasdf',
+                password_confirmation: 'asdfasdf',
+                application_status: 'not_applied')
+  end
+
+  test "user is valid" do
+    assert valid_user.valid?
+  end
+
+  test "user with valid/invalid application_status" do
+    user =  valid_user
+    assert user.valid?
+
+    user.application_status = 'gibberish'
+    assert_not user.valid?
+
+    user.application_status = 'not_applied'
+    assert user.valid?
+
+    user.application_status = 'applied'
+    assert user.valid?
+
+    user.application_status = 'approved'
+    assert user.valid?
+
+    user.application_status = 'rejected'
+    assert user.valid?
+
+    user.application_status = 'completed'
+    assert user.valid?
+  end
+
 end

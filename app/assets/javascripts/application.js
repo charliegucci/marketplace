c3eac3d7299b23fe2e1ba789ad09e194b2b839a5
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-03-11 14:05:52 +1000

    uploads screenshots,erds and code snippets for the documentation and added share-social gem for sharing listings in social medias

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,7 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+//= require social-share-button
 //= require jquery
 //= require rails-ujs
 //= require activestorage
